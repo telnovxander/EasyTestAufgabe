fe159116e7888a8b4034d7cd3531dd15b2464b41
telnovxander@gmail.com <telnovxander@gmail.com>
2026-08-18 21:24:19 +0200

    feat: Zeiterfassung pro Aufgabe (Blazor UI)

---
+++ src/EasyTestAufgabe.Web/Models/TimeEntryFormModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EasyTestAufgabe.Web.Models;
+
+/// <summary>
+/// Präsentationsmodell für das Erfassen eines Zeiteintrags im UI-Formular.
+/// </summary>
+public class TimeEntryFormModel
+{
+    public int TaskItemId { get; set; }
+
+    [Required(ErrorMessage = "Datum ist erforderlich.")]
+    public DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.Today);
+
+    [Range(1, 1440, ErrorMessage = "Dauer muss zwischen 1 und 1440 Minuten liegen.")]
+    public int DurationMinutes { get; set; } = 30;
+
+    [StringLength(500, ErrorMessage = "Notiz darf maximal 500 Zeichen lang sein.")]
+    public string? Note { get; set; }
+}
